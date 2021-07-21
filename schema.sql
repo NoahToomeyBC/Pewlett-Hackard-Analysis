5836b8fbe2460b1b6d8f1800870ec1d6b534c2a1
Nwtoomey <noahtoomeyBC@gmail.com>
2021-07-21 09:26:05 -0500

    Created table with employee names of those approaching retirement

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
+     dept_no VARCHAR(40) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
@@ -36,13 +36,12 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no TEXT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	    PRIMARY KEY (emp_no, dept_no)	
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE titles(
@@ -50,8 +49,6 @@ CREATE TABLE titles(
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL, 
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-		PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-SELECT * FROM departments;
